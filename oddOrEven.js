82593015d6fe04770cb3c0c34e9897d27e68a8ca
murkle1 <mdevera_@live.com>
2022-05-18 18:00:24 -0700

    initial commit

---
+++ oddOrEven.js
@@ -0,0 +1,14 @@
+function oddOrEven(arr) {
+    //sum of all elements
+    let sum = arr.reduce ((acc, c) => acc + Number(c), 0) 
+    // [1,2,3,4]
+    //10
+    // modulus the sum 
+    let mod = sum % 2
+    //if modulous = 0 then 'even' else 'odd' 
+    if(mod == 0){
+      return('even')
+    }else{
+      return('odd')
+    }
+  } 
