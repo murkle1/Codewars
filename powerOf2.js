2e20ae2670ee683f3fcf1227686d998d92d88900
murkle1 <mdevera_@live.com>
2022-06-10 08:48:04 -0700

    Update powerOf2.js
    fundamentals for each loop into an array

--- powerOf2.js
+++ powerOf2.js
@@ -1,5 +1,5 @@
 function powersOfTwo(n){
-    // loop starting at 0 and squares each ending with n. n = exponent
+    // loop starting at 0 and squares each ending with n. n = exponent.
     let arr = [];
     for(let i=0; i<=n; i++)
       arr[i] = 2**i;
