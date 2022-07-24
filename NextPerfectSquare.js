719d23065156fdc040a3b863ca1c0d1b3fb262b1
murkle1 <mdevera_@live.com>
2022-07-24 11:19:12 -0700

    fundamentals sqrt

---
+++ NextPerfectSquare.js
@@ -0,0 +1,10 @@
+function findNextSquare(sq) {
+    let num;
+    let nextNum;
+    if (Math.sqrt(sq) % 1 === 0){
+      num = Math.sqrt(sq)
+      nextNum = num + 1
+    }else{
+      return -1;
+    }
+    return nextNum*nextNum
