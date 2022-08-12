2a71b69399f4e0f746c89b290a38049428425400
murkle1 <mdevera_@live.com>
2022-08-12 09:42:56 -0700

    fundamentas var.toString()

---
+++ booleanToString.js
@@ -0,0 +1,13 @@
+function booleanToString(b){
+    if(b === true){
+      return "true"
+    }else{
+      return "false"
+    }
+  }
+
+  // OR 
+
+  function booleanToString(b){
+    return b.toString();
+  }
