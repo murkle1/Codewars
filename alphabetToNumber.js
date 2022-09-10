b4b0bbcb1209dccf7d430205023d7af9c679ad80
murkle1 <mdevera_@live.com>
2022-09-09 22:48:26 -0700

    .map and arrow function

---
+++ alphabetToNumber.js
@@ -0,0 +1,4 @@
+const switcher = x => (reverse = ' zyxwvutsrqponmlkjihgfedcba!? ',
+                      x.map(element => reverse[element]).join(''))
+
+//.map needs a function in it (and will return it into an array)
