28d5bd9172367240b96e3eb7e4362f0765621bb9
murkle1 <mdevera_@live.com>
2022-09-20 20:25:49 -0700

    if else if one liner

---
+++ ternaryOperator.js
@@ -0,0 +1,7 @@
+function checkEqual(a, b) {
+    return a === b ? "Equal" : "Not Equal";
+  }
+
+// The syntax is a ? b : c, where a is the condition, 
+// b is the code to run when the condition returns true,
+// and c is the code to run when the condition returns false.
