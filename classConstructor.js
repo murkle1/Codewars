de70188c3f65b381babce23e3ec76080e9e507c5
murkle1 <mdevera_@live.com>
2022-09-22 19:10:00 -0700

    create object with prop that gets passed to the contstructor

---
+++ classConstructor.js
@@ -0,0 +1,8 @@
+class Vegetable {
+    constructor(name) {
+      this.name = name;
+    }
+  }
+
+  //The Vegetable class allows you to create a vegetable
+  // object with a property name that gets passed to the constructor.
