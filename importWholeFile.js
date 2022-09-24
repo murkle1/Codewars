5fe8ce73817e068201776a293328fbaa1a9ace78
murkle1 <mdevera_@live.com>
2022-09-24 10:36:53 -0700

    importing whole files as objects into another .js file

---
+++ importWholeFile.js
@@ -0,0 +1,7 @@
+import * as stringFunctions from './string_functions.js';
+// Only change code above this line
+
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
+
+//stringFunctions is an object now with properties of uppercaseString and lowercaseString
