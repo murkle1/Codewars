ca1a40319a9dbe7a5b44f20ae0b450eef53249b0
murkle1 <mdevera_@live.com>
2022-09-24 10:19:34 -0700

    importing code from another file

---
+++ importCodeBlock.js
@@ -0,0 +1,7 @@
+import { uppercaseString, lowercaseString } from './string_functions.js'
+// Only change code above this line
+
+uppercaseString("hello");
+lowercaseString("WORLD!");
+
+// in a main.js file you can import functions from another file that has the 'export' syntax in it so you dont have to rewrite the function
