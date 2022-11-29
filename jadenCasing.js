a6509664474abe9cc02a829f8a09f90b83ca0d43
murkle1 <mdevera_@live.com>
2022-11-28 19:37:41 -0800

    split, map and then join. review keyword THIS

---
+++ jadenCasing.js
@@ -0,0 +1,7 @@
+String.prototype.toJadenCase = function () { 
+    return this.split(" ").map(function(word){
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(" ");
+  }
+  
+  // split and then map(function here) and then join
