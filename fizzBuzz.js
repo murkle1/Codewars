ccd8ffc0d4bd326472a001791e9cc7d680cdf7b4
Mark <mdevera_@live.com>
2023-01-03 16:30:50 -0500

    updated fizbuzz with for loop

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,6 +1,6 @@
 let array = [];
 let count = 1;
-
+// while loop
 function fizzBuzz(){
 
     while (count <=100){
@@ -16,5 +16,20 @@ function fizzBuzz(){
     }
 
     count++;
+    }
+}
+
+//for loop
+let array = [];
+let count = 1;
+for(let count = 1; count<101; count++){
+    if (count % 3 === 0 && count % 5 === 0) {
+        array.push("FizzBuzz");
+    } else if (count % 3 === 0) {
+        array.push("Fizz");
+    } else if (count % 5 === 0) {
+        array.push("Buzz");
+    } else {
+        array.push(count)
     }
 }
